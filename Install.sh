4d43e9b779039a3f28eeb1bca8a4c8eaa42ceab5
Renbull1994 <162243499+Renbull1994@users.noreply.github.com>
2024-04-15 13:18:29 +0800

    Update Install.sh

--- Install.sh
+++ Install.sh
@@ -4,148 +4,100 @@
 GREEN='\033[0;32m'
 NC='\033[0m' # 恢復為正常顏色
 
+
+echo "================================"
+echo "MySQL Community Server 8" 
+# DB Port
+echo -n "Install Port: "
+read DB_Port
+echo "================================"
+
+read -s -n1 -p "按任意键开始运行脚本 ... "
+echo ""
+
+
 # 創建安裝目錄
-mkdir -p /data/mysql_data
-mkdir -p /data/mysql_audit
-mkdir -p /data/mysql_audit_archive
-mkdir -p /data/myxtrabackup
-mkdir -p /data/mysql_tmpdir
+mkdir -p /home/mysql_$DB_Port/data
+mkdir -p /home/mysql_$DB_Port/log
 
-# 下載 Percona MySQL 安裝包
+# 下載安裝MySQL 8.0
+wget https://dev.mysql.com/get/Downloads/MySQL-8.0/mysql-8.0.36-1.el7.x86_64.rpm-bundle.tar
+tar -xf mysql-8.0.36-1.el7.x86_64.rpm-bundle.tar
+echo "Install MySQL.........."
+yum -y -q install mysql-community-libs*.rpm mysql-community-server-8.0*.rpm mysql-community-client-*.rpm mysql-community-common-8.*.rpm mysql-community-icu-data-files-8.*.rpm
+rm -rf mysql-community-*
+
+# 下載安裝Repo包及Xtrabackup套件
+echo "Install Xtrabackup.........."
 yum -y -q install https://repo.percona.com/yum/percona-release-latest.noarch.rpm
-
-# 選擇安裝的版本
-PS3="請選擇要安裝的Percona版本: "
-options=("8.0" "8.2" "Quit")
-select opt in "${options[@]}"
-do
-    case $opt in
-        "8.0")
-            PERCONA_VERSION="8.0"
-            break
-            ;;
-        "8.2")
-            PERCONA_VERSION="8.2"
-            break
-            ;;
-        "Quit")
-            echo "退出安裝"
-            exit 0
-            ;;
-        *) echo "無效選擇";;
-    esac
-done
-
-
-if [ "$opt" == "8.0" ]; then
-
 percona-release setup ps80
-percona-release enable tools release
-echo "Install Percona MySQL.........."
-yum -y -q install percona-server-server percona-mysql-shell percona-mysql-router
-
+yum -y -q install https://dl.fedoraproject.org/pub/epel/7/x86_64/Packages/z/zstd-1.5.5-1.el7.x86_64.rpm
+yum -y -q install percona-xtrabackup-80
 
 systemctl daemon-reload
 systemctl stop mysqld
+
+chown -R mysql:mysql /home/mysql_$DB_Port
 
 # 創建 Percona MySQL 配置文件
 echo "Creating Percona Server configuration..."
 cat <<EOF > /etc/my.cnf
 [mysqld]
-server_id=1
+port                                   = $DB_Port
+server_id                              = 1032$DB_Port
+character_set_server                   = utf8mb4
+collation_server                       = utf8mb4_unicode_ci
+basedir                                = /home/mysql_$DB_Port
+datadir                                = /home/mysql_$DB_Port/data
+slow_query_log                         = 1
+long_query_time                        = 3
+slow_query_log_file                    = /home/mysql_$DB_Port/slow.log
+log_error							   = /home/mysql_$DB_Port/log/mysqld.log
 
-datadir=/data/mysql_data
-relay-log=1-relay-log
-log_error=/data/mysql_data/1.err
-slow_query_log_file=/data/mysql_data/1-slow.log
+# replication
+log_replica_updates                    = 1
+binlog_expire_logs_seconds             = 2592000     # 3 days
+binlog_format                          = row
+max_binlog_size                        = 1G
+relay-log                              = mysql-relay-bin
+log-bin                                = mysql-bin
+#read_only                              = ON
 
-max_connections=20000
-table_open_cache=20000
-expire_logs_days=14
-gtid_mode=ON 
-log-bin=1-log-bin
-log-slave-updates
-binlog_format=ROW
-sync_binlog=1
-relay_log_recovery=on
-enforce-gtid-consistency
-slow_query_log=on
-long_query_time=0.2
-innodb_log_file_size=2G
-innodb_buffer_pool_size=2G
-master_info_repository=TABLE
-relay_log_info_repository=TABLE
-character_set_server=utf8
-innodb_flush_method=O_DIRECT
+# network
+max_allowed_packet                     = 128M
+back_log                               = 1024
+interactive_timeout                    = 600
+wait_timeout                           = 600
+skip_name_resolve                      = 1
+max_user_connections                   = 40000
+max_connections                        = 50000
+max_connect_errors                     = 100000
+sql_mode="STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION"
+
+# innodb
+innodb_buffer_pool_size                = 5G
+innodb_data_file_path                  = ibdata1:1G:autoextend
+innodb_max_dirty_pages_pct             = 50
+innodb_flush_method                    = O_DIRECT
+innodb_print_all_deadlocks             = 1
+innodb_print_ddl_logs                  = 1
+innodb_log_buffer_size                 = 16M
 EOF
 
-# 初始化 Percona MySQL 數據庫
-echo "Initializing Percona Server database..."
-mysqld --initialize --user=mysql
+# 初始化 MySQL MySQL 數據庫
+echo "Initializing MySQL Server database..."
+mysqld --initialize --user=mysql 
+PASSWD=$(grep temporary /home/mysql_$DB_Port/log/mysqld.log | awk '{print $NF}')
 
-PASSWD=$(grep temporary /data/mysql_data/1.err | awk '{print $NF}')
-echo -e "Temp password = ${GREEN}$PASSWD${NC}"
 
-# 啟動 Percona MySQL 服務
-echo "Starting Percona Server service..."
+# 啟動 MySQL 服務
+echo "Starting MySQL Server service..."
 systemctl start mysqld
 systemctl enable mysqld
 
-echo "Percona Server installation completed."
+echo "MySQL Server installation completed."
 
-elif [ "$opt" == "8.2" ]; then
-
-percona-release enable-only ps-8x-innovation release
-percona-release enable tools release
-echo "Install Percona MySQL.........."
-yum -y -q install percona-server-server percona-mysql-shell percona-mysql-router
-
-
-systemctl daemon-reload
-systemctl stop mysqld
-
-# 創建 Percona MySQL 配置文件
-echo "Creating Percona Server configuration..."
-cat <<EOF > /etc/my.cnf
-[mysqld]
-server_id=1
-
-datadir=/data/mysql_data
-relay-log=1-relay-log
-log_error=/data/mysql_data/1.err
-slow_query_log_file=/data/mysql_data/1-slow.log
-
-max_connections=20000
-table_open_cache=20000
-gtid_mode=ON 
-log-bin=1-log-bin
-log-slave-updates
-binlog_format=ROW
-sync_binlog=1
-relay_log_recovery=on
-enforce-gtid-consistency
-slow_query_log=on
-long_query_time=0.2
-innodb_log_file_size=2G
-innodb_buffer_pool_size=2G
-master_info_repository=TABLE
-relay_log_info_repository=TABLE
-character_set_server=utf8
-innodb_flush_method=O_DIRECT
-EOF
-
-# 初始化 Percona MySQL 數據庫
-echo "Initializing Percona Server database..."
-mysqld --initialize --user=mysql
-
-PASSWD=$(grep temporary /data/mysql_data/1.err | awk '{print $NF}')
+echo "**********************************"
+echo "Login cmd : mysql -u root -p'"
 echo -e "Temp password = ${GREEN}$PASSWD${NC}"
-
-# 啟動 Percona MySQL 服務
-echo "Starting Percona Server service..."
-systemctl start mysqld
-systemctl enable mysqld
-
-echo "Percona Server installation completed."
-
-fi
+echo "**********************************"
